4854216eec839e67487bbbe08a7f683b52e60378
VeronikaMishkovec <veronikakireenko@gmail.com>
2019-03-20 21:15:27 +0300

    feat:add balanced for ||

--- src/index.js
+++ src/index.js
@@ -1,64 +1,78 @@
-module.exports = function check(str, bracketsConfig) {
-    // your solution
-    var openBr = '';
-    var closeBr = '';
-    for (var i = 0; i < bracketsConfig.length; i++) {
-        for (var j = 0; j < bracketsConfig[i].length; j++) {
-            if (j == 0) {
-                openBr += bracketsConfig[i][j];
-            } else { closeBr += bracketsConfig[i][j]; }
-        }
-    }
-    //alert(openBr + ' ' + closeBr);
-    var open = 0;
-    var close = 0;
+ module.exports = function check(str, bracketsConfig) {
+     var openBr = '';
+     var closeBr = '';
+     for (let i = 0; i < bracketsConfig.length; i++) {
+         for (let j = 0; j < bracketsConfig[i].length; j++) {
+             if (j == 0) {
+                 openBr += bracketsConfig[i][j];
+             } else { closeBr += bracketsConfig[i][j]; }
+         }
+     }
+     var stack = [];
+     var configArr = openBr.concat(closeBr);
+     for (let i = 0; i < str.length; i++) {
+         for (let j = 0; j < configArr.length / 2; j++) {
+             if (str[i] === configArr[j]) {
+                 stack.push(str[i]);
+             }
 
-    for (var k = 0; k < str.length; k++) {
-        for (var m = 0; m < openBr.length; m++) {
-            if (openBr[m] == str[k]) {
-                open++;
-            }
-        }
-        for (var n = 0; n < closeBr.length; n++) {
-            if (closeBr[n] == str[k]) {
-                close++;
-            }
-        }
-    }
-    if (open != close) {
-        return false;
-    } else { return true; }
-}
 
-const config1 = [
-    ['(', ')']
-];
-const config2 = [
-    ['(', ')'],
-    ['[', ']']
-];
-const config3 = [
-    ['(', ')'],
-    ['[', ']'],
-    ['{', '}']
-];
-const config4 = [
-    ['|', '|']
-];
-const config5 = [
-    ['(', ')'],
-    ['|', '|']
-];
-const config6 = [
-    ['1', '2'],
-    ['3', '4'],
-    ['5', '6'],
-    ['7', '7'],
-    ['8', '8']
-];
-const config7 = [
-    ['(', ')'],
-    ['[', ']'],
-    ['{', '}'],
-    ['|', '|']
-];
+             if (str[i] != configArr[j]) {
+                 if (str[i] == configArr[j + configArr.length / 2] && str[i - 1] == configArr[j]) {
+                     stack.pop();
+                 }
+             }
+
+             if (str[i] != configArr[j]) {
+                 if (str[i] == configArr[j + configArr.length / 2] && str[i - 1] == configArr[j]) {
+                     stack.pop();
+                 }
+             }
+
+             if (str[i] != configArr[j]) {
+                 if (str[i] == configArr[j + configArr.length / 2] && str[i - 1] == configArr[j]) {
+                     stack.pop();
+                 }
+             }
+             if (configArr[j] == configArr[j + configArr.length / 2]) {
+                 stack.pop();
+             }
+         }
+     }
+     if (stack.length === 0) {
+         return document.write('true');
+     } else { return document.write('false'); }
+
+ }
+ const config1 = [
+     ['(', ')']
+ ];
+ const config2 = [
+     ['(', ')'],
+     ['[', ']']
+ ];
+ const config3 = [
+     ['(', ')'],
+     ['[', ']'],
+     ['{', '}']
+ ];
+ const config4 = [
+     ['|', '|']
+ ];
+ const config5 = [
+     ['(', ')'],
+     ['|', '|']
+ ];
+ const config6 = [
+     ['1', '2'],
+     ['3', '4'],
+     ['5', '6'],
+     ['7', '7'],
+     ['8', '8']
+ ];
+ const config7 = [
+     ['(', ')'],
+     ['[', ']'],
+     ['{', '}'],
+     ['|', '|']
+ ];
